dcd58ad6f61f7b60ef421f7dbdde536999703801
sereginn <mr.sereginn@mail.ru>
2015-02-26 20:57:59 +0400

    Второе домашнее задание 2/3

--- www/index.php
+++ www/index.php
@@ -1,9 +1,7 @@
 <?php
+require_once __DIR__ . '/autoload.php';
 session_start();
 $_SESSION['ctrl']=isset($_GET['ctrl'])? $_GET['ctrl'] : 'News';
 $_SESSION['act']=isset($_GET['act'])? $_GET['act'] : 'All';
 
-
-var_dump($_SESSION['ctrl']); var_dump($_SESSION['act']);
-require __DIR__ . '/classes/selectionOption.php';
-SelectiOnoption::view();
+SelectionOption::view();
