8d67340049839bac5d0cabf2ad2f825d2c027d57
sereginn <mr.sereginn@mail.ru>
2015-02-27 13:49:32 +0400

    Второе домашнее задание 2/3

--- www/autoload.php
+++ www/autoload.php
@@ -2,11 +2,14 @@
 function __autoload($class)
 {
     if (file_exists(__DIR__. '/controllers/' . $class . '.php'))
-    {
-      require __DIR__. '/controllers/' . $class . '.php';
-    }
+{
+    require __DIR__. '/controllers/' . $class . '.php';
+}
     if (file_exists(__DIR__. '/classes/' . $class . '.php'))
     {
         require __DIR__. '/classes/' . $class . '.php';
+    }
+    if (file_exists(__DIR__. '/models/' . $class . '.php')) {
+        require __DIR__ . '/models/' . $class . '.php';
     }
 }
--- www/controllers/index.php
+++ www/controllers/index.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-
+require_once __DIR__ . '/../autoload.php';
 $contr=$_SESSION['ctrl'];
 $act=$_SESSION['act'];
 
 $ControllerClassName= $contr . 'Controller';
-require_once __DIR__ . '/' . $ControllerClassName . '.php';
+
 $metod='action'. $act;
 $ControllerClassName ::$metod();
-var_dump($ControllerClassName);
